2f0b9eaab6f0f773b66286141f821a7b4018fa9c
Rebecca Smith <2145912+Rebeccals@users.noreply.github.com>
2024-03-08 07:54:24 -0800

    Add files via upload
    Added new files

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php require_once("./core/functions.php"); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>HTML 5 Boilerplate</title>
+    <link rel="stylesheet" href="style.css">
+
+    <nav class="menu">
+        <?php require_once("./core/navigation.php"); ?>
+    </nav>
+
+</head>
+
+<body>
+    <?php require_once("./core/body.php"); ?>
+    <script src="index.js"></script>
+</body>
+
+<footer>
+    <?php require_once("./core/footer.php"); ?>
+</footer>
+
+</html>
